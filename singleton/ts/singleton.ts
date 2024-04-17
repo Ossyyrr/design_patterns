e90dbe77333c286b4f2496f9ff4948f0b2e7f32a
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-04-17 09:03:41 +0200

    singletone with ts

--- singleton/ts/singleton.ts
+++ singleton/ts/singleton.ts
@@ -4,3 +4,26 @@
 // ts-node singleton.ts
 
 console.log("Singleton Pattern");
+
+class SingletonTs {
+  private static instance: SingletonTs;
+  public random: number;
+
+  // Private constructor for protect the instance
+  private constructor() {
+    this.random = Math.random();
+  }
+
+  public static getInstance(): SingletonTs {
+    if (!SingletonTs.instance) {
+      SingletonTs.instance = new SingletonTs();
+    }
+    return SingletonTs.instance;
+  }
+}
+
+const singleton = SingletonTs.getInstance();
+const singleton2 = SingletonTs.getInstance();
+
+console.log(singleton.random);
+console.log(singleton2.random);
