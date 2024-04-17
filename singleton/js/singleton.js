422b05c91720b45b6a01d6a9befbf824b348941a
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-04-17 08:50:12 +0200

    singleton example

--- singleton/js/singleton.js
+++ singleton/js/singleton.js
@@ -15,3 +15,28 @@ class Singleton{
 
 const singleton = new Singleton();
 const singleton2 = new Singleton();
+
+console.log('************');
+
+class WeekDays{
+    daysEs = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+    daysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    constructor(lang){
+        this.lang = lang;
+        if(WeekDays.instance){
+            return WeekDays.instance;
+        }
+        WeekDays.instance = this;
+    }
+
+    getDays(){
+        return this.lang === 'es' ? this.daysEs : this.daysEn;
+    }
+}
+
+const weekDays = new WeekDays('es') ;
+const weekDays2 = new WeekDays('es') ;
+
+console.log(weekDays.getDays());
+console.log(weekDays2.getDays());
---
+++ singleton/ts/singleton.ts
@@ -0,0 +1,6 @@
+// Para ejecutar con la consola usar el plugin ts-node (npm i -g ts-node)
+// cd singleton
+// cd ts
+// ts-node singleton.ts
+
+console.log("Singleton Pattern");
